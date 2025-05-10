2e6d7e2038f1dbc34635cf41efd467e08bbb9dea
Karan Negi <142792314+KaranNegi20Feb@users.noreply.github.com>
2025-05-10 20:37:42 +0530

    Completed API for getUsersOrganizations

--- backend/src/graphql/user/queries.ts
+++ backend/src/graphql/user/queries.ts
@@ -2,4 +2,5 @@ export const queries=`#graphql
     getUserToken(email:String!,password:String!):String
     getAllUsers: [User!]!
     getUserTokenWithGoogle(credential: String!): String
+    getUsersOrganizations(credential:String!):[Organization!]!
 `
