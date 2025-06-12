e208cddc801dc478487d7a9ec77a181f0a4a476c
Karan Negi <142792314+KaranNegi20Feb@users.noreply.github.com>
2025-06-12 22:40:45 +0530

    sorting issues 1

--- frontend/tasker-saas-frontend/src/components/Navbar.tsx
+++ frontend/tasker-saas-frontend/src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Button } from "../components/ui/button";
+import { Button } from "./ui/button";
 import { Menu } from "lucide-react";
 
 export const Navbar = () => {
